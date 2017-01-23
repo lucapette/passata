0e354224adc06b44023a7be760e0e03c347d2e3c
lucapette <lucapette@gmail.com>
2017-01-23 09:09:48 +0100

    Added babel/webpack config
    Babel works on its own but not via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'App.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: 'node_modules',
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      }
+    ]
   }
 }
 
