66a6caee601a7ef13c7385efa64f7425fdd28d2f
lucapette <lucapette@gmail.com>
2017-01-24 17:31:53 +0100

    Introduce a layout component so we can start putting css in place

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function world(): string {
-  return "world"
-}
+import Layout from './Layout';
 
-ReactDOM.render(
-  <h1>Hello {world()}</h1>,
-  document.getElementById('root')
-);
+const mountNode = document.getElementById('root');
+
+ReactDOM.render(<Layout />, mountNode);
