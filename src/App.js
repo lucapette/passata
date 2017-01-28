ab53953a71c208499052b82c29ff3a968b2f9fe4
lucapette <lucapette@gmail.com>
2017-01-28 23:18:01 +0100

    Add semantic ui react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layout from './Layout';
+import { Button } from 'semantic-ui-react'
+
+const ButtonExampleEmphasis = () => (
+  <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+  </div>
+)
 
 const mountNode = document.getElementById('root');
 
-ReactDOM.render(<Layout />, mountNode);
+ReactDOM.render(<ButtonExampleEmphasis />, mountNode);
