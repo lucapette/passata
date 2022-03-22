ebff4a5f840c203d92db5ceda0c947fc7b638e91
lucapette <ciao@lucapette.me>
2022-03-22 17:07:57 +0100

    Basic buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,61 @@
 import "bulma/css/bulma.min.css";
+import { PlayCircle, StopCircle } from "lucide-react";
+import React, { useState } from "react";
+import { Button, Columns, Content, Form, Icon } from "react-bulma-components";
+
+enum TimerState {
+  NOT_READY,
+  READY,
+  RUNNING,
+  DONE,
+}
 
 const Home = () => {
-  return <div>home</div>;
+  const [timerState, setTimerState] = useState(TimerState.NOT_READY);
+  const [topic, setTopic] = useState("");
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") {
+      setTimerState(TimerState.RUNNING);
+      setTopic("");
+      console.log(`starting pomodoro with ${topic}`);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTopic(e.currentTarget.value);
+    setTimerState(
+      e.currentTarget.value.length > 0 ? TimerState.READY : TimerState.NOT_READY
+    );
+  };
+  return (
+    <Columns>
+      <Columns.Column size="one-third">
+        <Content>
+          <Form.Input
+            placeholder="What are you working on?"
+            onKeyDown={onKeyDown}
+            onChange={onChange}
+            value={topic}
+            disabled={timerState === TimerState.RUNNING}
+          />
+        </Content>
+        <Button.Group>
+          <Button disabled={timerState !== TimerState.READY}>
+            <Icon>
+              <PlayCircle />
+            </Icon>
+          </Button>
+
+          <Button disabled={timerState !== TimerState.RUNNING}>
+            <Icon>
+              <StopCircle />
+            </Icon>
+          </Button>
+        </Button.Group>
+      </Columns.Column>
+    </Columns>
+  );
 };
 
 export default Home;
